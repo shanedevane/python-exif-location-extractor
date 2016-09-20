608b5c575ab1a85b7f41f44fc4dde9389ac5edb4
Shane Devane (SD-WIN) <shanedevane@gmail.com>
2016-09-20 10:42:24 +0100

    initial extractor class to check EXIF data for gps

--- README.md
+++ README.md
@@ -1,40 +1,20 @@
-===============================
-Python EXIF Location Extractor
-===============================
+# Python EXIF Location Extractor
 
 
-.. image:: https://img.shields.io/pypi/v/python_exif_location_extractor.svg
-        :target: https://pypi.python.org/pypi/python_exif_location_extractor
-
-.. image:: https://img.shields.io/travis/shanedevane/python_exif_location_extractor.svg
-        :target: https://travis-ci.org/shanedevane/python_exif_location_extractor
-
-.. image:: https://readthedocs.org/projects/python-exif-location-extractor/badge/?version=latest
-        :target: https://python-exif-location-extractor.readthedocs.io/en/latest/?badge=latest
-        :alt: Documentation Status
-
-.. image:: https://pyup.io/repos/github/shanedevane/python_exif_location_extractor/shield.svg
-     :target: https://pyup.io/repos/github/shanedevane/python_exif_location_extractor/
-     :alt: Updates
 
 
-Small wrapper class around Pillow to Extract location data from a JPEG
 
 
-* Free software: MIT license
-* Documentation: https://python-exif-location-extractor.readthedocs.io.
 
 
-Features
---------
 
-* TODO
 
-Credits
----------
+# Reference
 
-This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+## OTHER USEFUL ITEMS
 
-.. _Cookiecutter: https://github.com/audreyr/cookiecutter
-.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+### Cookiecutter for initial project files and configuration
+https://github.com/audreyr/cookiecutter-pypackage
 
+### Auto generate requirements file 'correctly'
+http://www.idiotinside.com/2015/05/10/python-auto-generate-requirements-txt/
--- python_exif_location_extractor/python_exif_location_extractor.py
+++ python_exif_location_extractor/python_exif_location_extractor.py
@@ -1,1 +1,66 @@
 # -*- coding: utf-8 -*-
+
+import os
+import exifread
+from PIL import Image, ImageStat, ImageChops, ImageEnhance
+from PIL.ExifTags import TAGS, GPSTAGS
+
+
+class PythonExifLocationExtractor:
+
+    def __init__(self, file_path):
+        self._file_path = file_path
+        self.json = None
+        self._json_data = dict()
+        self._image = None
+        self._pil_tags = dict()
+
+    def _get_exif_data(self):
+        if self._image._getexif():
+            for tag in self._image._getexif():
+                try:
+                    decoded = TAGS.get(tag, tag)
+                    self._pil_tags[decoded] = self._image._getexif().get(tag)
+                except AttributeError as e:
+                    # print("AttributeError" + str(e))
+                    pass
+
+        try:
+            exif = exifread.process_file(self._image, details=True)
+            self._exif_read_tags = exif
+        except AttributeError as e:
+            # print("AttributeError" + str(e))
+            pass
+        except MemoryError as e:
+            # print("MemoryError" + str(e))
+            pass
+
+    def _get_gps_data(self):
+        gps_exif_data = self._pil_tags.get('GPSInfo', None)
+        if gps_exif_data:
+            gps_data = {}
+            for t in gps_exif_data:
+                sub_decoded = GPSTAGS.get(t, t)
+                gps_data[sub_decoded] = gps_exif_data[t]
+            self._gps_data = gps_data
+
+    def _store_gps_data(self):
+        if self._get_gps_data:
+            # self._json_data
+            pass
+
+    def execute(self):
+        self._image = Image.open(self._file_path)
+        self._get_exif_data()
+        self._get_gps_data()
+        self._store_gps_data()
+
+
+if __name__ == "__main__":
+    extractor = PythonExifLocationExtractor('../Resources/pacers.jpg')
+    extractor.execute()
+    print(extractor.json)
+
+
+
+
